557af968cd95321dea296f3c1a54def30e0fe26d
mickeyyo55b <mickeyyo55b@gmail.com>
2024-04-10 21:04:15 +0800

    04102104

--- eeit82v2/src/tw/org/iii/tutor/HelloWorld.java
+++ eeit82v2/src/tw/org/iii/tutor/HelloWorld.java
@@ -2,7 +2,7 @@ package tw.org.iii.tutor;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, World111");
+		System.out.println("Hello, World1111");
 		System.out.print("Hello, World2");
 	}
 }
