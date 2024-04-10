b2c1effd84178f2ff6335bb729df2eda6bb2eeec
mickeyyo55b <mickeyyo55b@gmail.com>
2024-04-10 14:32:00 +0800

    04101431

---
+++ eeit82v2/src/tw/org/iii/tutor/Brad04.java
@@ -0,0 +1,27 @@
+package tw.org.iii.tutor;
+
+public class Brad04 {
+
+	public static void main(String[] args) {
+		double temp = Math.random();
+		//System.out.println(temp);
+		int score = (int)(temp * 101);
+		System.out.println(score);
+		
+		if (score >= 90) {
+			System.out.println("A");
+		}else if (score >= 80) {
+			System.out.println("B");
+		}else if (score >= 70) {
+			System.out.println("C");
+		}else if (score >= 60) {
+			System.out.println("D");
+		}else {
+			System.out.println("E");
+		}						
+							
+					
+		
+	}
+
+}
