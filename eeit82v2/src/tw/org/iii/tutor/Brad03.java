2635afc8de19bad9919a39a9d951926b82ac7277
mickeyyo55b <mickeyyo55b@gmail.com>
2024-04-10 13:48:12 +0800

    04101348

---
+++ eeit82v2/src/tw/org/iii/tutor/Brad03.java
@@ -0,0 +1,19 @@
+package tw.org.iii.tutor;
+
+public class Brad03 {
+
+	public static void main(String[] args) {
+		float var1 = 10;
+		float var2 = 12.3f;
+		double var3 = 12.3;
+		
+		char c1 = 'a';
+		char c2 = 65;
+		System.out.println(c2);
+		
+		boolean b1 = true;
+		boolean b2 = false;
+
+	}
+
+}
