c98ed638fece0b8408077574ac5616e2ff2cc149
mickeyyo55b <mickeyyo55b@gmail.com>
2024-04-15 16:12:04 +0800

    04151611

--- eeit82v2/src/tw/org/iii/apis/TWId.java
+++ eeit82v2/src/tw/org/iii/apis/TWId.java
@@ -7,17 +7,26 @@ public class TWId {
 		// 3. 2 => 1 / 2
 		// 4. 3 - 10 => 0-9
 		boolean isRight = false;
-		if (id.length() == 10) {
+		if (id.matches("[A-Z][12][0-9]{8}")) {
+			String letters = "ABCDEFGHJKLMNPQRSTUVXYWZIO";
 			char c1 = id.charAt(0);
-//			if (c1 >= 65 && c1 <= 90) {
-//				isRight = true;				
-//			}
-			String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-			if (letters.indexOf(c1) != -1) {
-				
-			}
+			int a12 = letters.indexOf(c1) + 10;
+			int a1 = a12 / 10;
+			int a2 = a12 % 10;
+			int n1 = Integer.parseInt(id.substring(1, 2)) ;
+			int n2 = Integer.parseInt(id.substring(2, 3)) ;
+			int n3 = Integer.parseInt(id.substring(3, 4)) ;
+			int n4 = Integer.parseInt(id.substring(4, 5)) ;
+			int n5 = Integer.parseInt(id.substring(5, 6)) ;
+			int n6 = Integer.parseInt(id.substring(6, 7)) ;
+			int n7 = Integer.parseInt(id.substring(7, 8)) ;
+			int n8 = Integer.parseInt(id.substring(8, 9)) ;
+			int n9 = Integer.parseInt(id.substring(9, 10)) ;
+			int n = a1*1 + a2*9 + n1*8 + n2 *7 + n3*6 + n4*5 + n5*4 + n6*3 + 
+					n7*2 + n8*1 + n9*1;
+			
+			isRight = n % 10 == 0;
 		}
-		System.out.println(id.length());
 		
 		
 		
--- eeit82v2/src/tw/org/iii/tutor/Brad20.java
+++ eeit82v2/src/tw/org/iii/tutor/Brad20.java
@@ -5,7 +5,7 @@ import tw.org.iii.apis.TWId;
 public class Brad20 {
 
 	public static void main(String[] args) {
-		if (TWId.checkTWId("資策會")) {
+		if (TWId.checkTWId("A123456789")) {
 			System.out.println("OK");
 		}else {
 			System.out.println("XX");
