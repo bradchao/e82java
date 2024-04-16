cd3b3f0f589f9af870096e4df8a8c4487c94712d
mickeyyo55b <mickeyyo55b@gmail.com>
2024-04-16 12:01:40 +0800

    04161201

--- eeit82v2/src/tw/org/iii/tutor/Brad22.java
+++ eeit82v2/src/tw/org/iii/tutor/Brad22.java
@@ -1,7 +1,26 @@
 package tw.org.iii.tutor;
 public class Brad22 {
 	public static void main(String[] args) {
-
+		Brad224 obj1 = new Brad224();
+		obj1.m1();
+		Brad221 obj2 = new Brad221();
+		Brad221 obj3 = new Brad222();
+		Brad221 obj4 = new Brad224();
+		obj4.m1();
+		Brad224 obj5 = (Brad224)obj4;
+		obj5.m4();
+		//Brad223 obj6 = (Brad223)obj4;
+		
+		ma(obj1);
+	}
+	
+	static void ma(Brad221 obj) {
+		obj.m1();
+		if (obj instanceof Brad224) {
+			((Brad224)obj).m4();
+		}else if (obj instanceof Brad222) {
+			((Brad222)obj).m2();
+		}
 	}
 }
 
@@ -11,10 +30,16 @@ class Brad221 {
 }
 class Brad222 extends Brad221 {
 	Brad222(){System.out.println("Brad222");}
+	void m1() {System.out.println("Brad222:m1()");}
+	void m2() {System.out.println("Brad222:m2()");}
 }
 class Brad223 extends Brad221 {
 	Brad223(){System.out.println("Brad223");}
+	void m1() {System.out.println("Brad223:m1()");}
+	void m3() {System.out.println("Brad223:m3()");}
 }
 class Brad224 extends Brad222 {
 	Brad224(){System.out.println("Brad224");}
+	void m1() {System.out.println("Brad224:m1()");}
+	void m4() {System.out.println("Brad224:m4()");}
 }
