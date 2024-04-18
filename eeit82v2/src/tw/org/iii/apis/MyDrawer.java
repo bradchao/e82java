2558edca8977d0de24166ec4c6b2993f41b141a3
mickeyyo55b <mickeyyo55b@gmail.com>
2024-04-18 10:15:03 +0800

    04181014

--- eeit82v2/src/tw/org/iii/apis/MyDrawer.java
+++ eeit82v2/src/tw/org/iii/apis/MyDrawer.java
@@ -4,27 +4,65 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.LinkedList;
 
 import javax.swing.DebugGraphics;
 import javax.swing.JPanel;
 
 public class MyDrawer extends JPanel{
+	private LinkedList<LinkedList<Point>> lines;
+	
+	
 	public MyDrawer() {
+		lines = new LinkedList<>();
+		
 		setBackground(Color.yellow);
+		MyMouseListener listener = new MyMouseListener();
+		addMouseListener(listener);
+		addMouseMotionListener(listener);
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		System.out.println("OK1");
 		
 		Graphics2D g2d = (Graphics2D)g;
 		
 		g2d.setColor(Color.blue);
-		g2d.setStroke(new BasicStroke(10));
-		g2d.drawLine(0, 0, 200, 400);
+		g2d.setStroke(new BasicStroke(4));
 
-	
+		for (LinkedList<Point> line : lines) {
+			for (int i=1; i<line.size(); i++) {
+				Point p0 = line.get(i-1);
+				Point p1 = line.get(i);
+				g2d.drawLine(p0.getX(), p0.getY(), p1.getX(), p1.getY());
+			}
+		}
+		
+		
 	}
+	
+	private class MyMouseListener extends MouseAdapter {
+		@Override
+		public void mousePressed(MouseEvent e) {
+			Point point = new Point(e.getX(), e.getY());
+			LinkedList<Point> line = new LinkedList<>();
+			
+			line.add(point);
+			lines.add(line);
+		}
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			Point point = new Point(e.getX(), e.getY());
+			lines.getLast().add(point);
+			
+			repaint();
+		}
+	}
+	
+	
+	
 	
 }
