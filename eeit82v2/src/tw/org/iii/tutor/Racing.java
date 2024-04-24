02dd8bd9e055a10356c109d02f89e6bc66e36f32
mickeyyo55b <mickeyyo55b@gmail.com>
2024-04-24 14:15:31 +0800

    04241415

--- eeit82v2/src/tw/org/iii/tutor/Racing.java
+++ eeit82v2/src/tw/org/iii/tutor/Racing.java
@@ -12,6 +12,7 @@ public class Racing extends JFrame{
 	private JButton go;
 	private JLabel[] lanes;
 	private Car[] cars;
+	private int rank;
 	
 	public Racing() {
 		super("Racing");
@@ -39,10 +40,19 @@ public class Racing extends JFrame{
 	}
 	
 	private void go() {
+		go.setEnabled(false);
+		initGame();
 		for(int i=0; i<cars.length; i++) cars[i] = new Car(i);
 		for (int i = 0; i<cars.length; i++) {
 			cars[i].start();
 		}
+	}
+	
+	private void initGame() {
+		for (int i=0; i<lanes.length; i++) {
+			lanes[i].setText(String.format("%d.", (i+1)));
+		}
+		rank = 0;
 	}
 	
 	class Car extends Thread {
@@ -53,9 +63,27 @@ public class Racing extends JFrame{
 		@Override
 		public void run() {
 			for (int i=0; i<100; i++) {
-				lanes[lane].setText(lanes[lane].getText() + ">");
+				if (i == 99) {
+					lanes[lane].setText(lanes[lane].getText() + "> WINNER" );
+					stopGame();
+				}else {
+					lanes[lane].setText(lanes[lane].getText() + ">");					
+				}
+				try {
+					Thread.sleep(10 + (int)(Math.random()*200));
+				} catch (InterruptedException e) {
+					//
+					break;
+				}
 			}
 		}
+	}
+	
+	private void stopGame() {
+		for (int i=0; i<cars.length; i++) {
+			cars[i].interrupt();
+		}
+		go.setEnabled(true);
 	}
 	
 	public static void main(String[] args) {
