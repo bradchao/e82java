c5851adb5c385d8e696bf891d0c492cf1b139a7f
mickeyyo55b <mickeyyo55b@gmail.com>
2024-04-25 10:09:32 +0800

    04251009

--- eeit82v2/src/tw/org/iii/tutor/MyBallGame.java
+++ eeit82v2/src/tw/org/iii/tutor/MyBallGame.java
@@ -14,9 +14,21 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MyBallGame extends JFrame{
-	private int x, y;
-	private Timer timer;
+	private BufferedImage[] balls;
+	private int viewW, viewH;
+	
 	public MyBallGame() {
+		
+		balls = new BufferedImage[4];
+		try {
+			balls[0] = ImageIO.read(new File("dir4/ball0.png"));
+			balls[1] = ImageIO.read(new File("dir4/ball1.png"));
+			balls[2] = ImageIO.read(new File("dir4/ball2.png"));
+			balls[3] = ImageIO.read(new File("dir4/ball3.png"));
+		}catch(Exception e) {
+			System.out.println(e); System.exit(0);
+		}
+		
 		
 		setLayout(new BorderLayout());
 		add(new MyPanel(), BorderLayout.CENTER);
@@ -25,40 +37,65 @@ public class MyBallGame extends JFrame{
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		timer = new Timer();
-		timer.schedule(new BallTask(), 1*1000, 60);
 	}
 	
 	private class MyPanel extends JPanel {
+		private Timer timer;
+		
 		public MyPanel() {
 			setBackground(Color.YELLOW);
+			timer = new Timer();
+			timer.schedule(new RefreshView(), 0, 16);
 		}
 		@Override
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
+			viewW = getWidth(); viewH = getHeight();
 			
 			Graphics2D g2d = (Graphics2D)g;
-			try {
-				BufferedImage ball = ImageIO.read(new File("dir4/ball.png"));
-				g2d.drawImage(ball, x, y, null);
-			}catch(Exception e) {
-				System.out.println(e);
+			
+			
+		}	
+		
+		private class RefreshView extends TimerTask {
+			@Override
+			public void run() {
+				repaint();
 			}
-			
-			
-		}		
+		}
+		
+		
 	}
+	
 	
 	private class BallTask extends TimerTask {
+		private int x, y, w, h, dx, dy;
+		private BufferedImage img;
+		
+		public BallTask(int x, int y) {
+			this.x = x; this.y = y;
+			int rand = (int)(Math.random()*4);	// 0 - 3
+			img = balls[rand];
+			w = img.getWidth(); h = img.getHeight();
+			dx = (int)(Math.random()*17) - 8;
+			dy = (int)(Math.random()*17) - 8;
+		}
+		
 		@Override
 		public void run() {
-			x += 4;
-			y += 4;
-			repaint();
+			if (x + w >= viewW || x <= 0) {
+				dx *= -1;
+			}
+			if (y + h >= viewH || y <= 0) {
+				dy *= -1;
+			}
+			x += dx;
+			y += dy;
 		}
+		public int getX() {return x;}
+		public int getY() {return y;}
+		public BufferedImage getImg() {return img;}
 	}
-	
-
 	
 	public static void main(String[] args) {
 		new MyBallGame();
