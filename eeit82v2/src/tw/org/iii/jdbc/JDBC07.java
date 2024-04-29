b5603cc2f3f822f976722458b6a770cd2026eb8f
mickeyyo55b <mickeyyo55b@gmail.com>
2024-04-29 11:41:37 +0800

    04291141

--- eeit82v2/src/tw/org/iii/jdbc/JDBC07.java
+++ eeit82v2/src/tw/org/iii/jdbc/JDBC07.java
@@ -2,6 +2,7 @@ package tw.org.iii.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Properties;
@@ -14,21 +15,31 @@ public class JDBC07 {
 		System.out.print("關鍵字: ");
 		Scanner scanner = new Scanner(System.in);
 		String key = scanner.next();
+		String kw = "%" + key + "%";
 		
 		
 		Properties prop = new Properties();
 		prop.put("user", "root");
 		prop.put("password", "root");
 		
-		String sql = "SELECT * FROM food";
+		String sql = "SELECT * FROM food WHERE name LIKE ? OR addr LIKE ? OR city LIKE ? OR town LIKE ?";
 		// jdbc:sqlserver://localhost:1433;databaseName=AdventureWorks;user=MyUserName;password=*****;encrypt=false;
 		try (Connection conn = DriverManager.getConnection(
 				"jdbc:mysql://localhost/iii", prop);
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
+			PreparedStatement pstmt = conn.prepareStatement(sql);
 				){
-			
-
+			pstmt.setString(1, kw);
+			pstmt.setString(2, kw);
+			pstmt.setString(3, kw);
+			pstmt.setString(4, kw);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				String name = rs.getString("name");
+				String addr = rs.getString("addr");
+				String city = rs.getString("city");
+				String town = rs.getString("town");
+				System.out.printf("%s:%s:%s:%s\n",name,addr,city,town);
+			}
 			
 			
 		}catch(Exception e) {
