701f8c322f17f2af1c030e164c9bf3cfedbcd82b
mickeyyo55b <mickeyyo55b@gmail.com>
2024-04-29 11:59:19 +0800

    04291159

--- eeit82v2/src/tw/org/iii/jdbc/JDBC08.java
+++ eeit82v2/src/tw/org/iii/jdbc/JDBC08.java
@@ -8,41 +8,31 @@ import java.sql.Statement;
 import java.util.Properties;
 import java.util.Scanner;
 
-public class JDBC07 {
+public class JDBC08 {
 
 	public static void main(String[] args) {
 		//
-		System.out.print("關鍵字: ");
+		System.out.print("頁: ");
 		Scanner scanner = new Scanner(System.in);
-		String key = scanner.next();
-		String kw = "%" + key + "%";
-		
+		int page = scanner.nextInt();
 		
 		Properties prop = new Properties();
 		prop.put("user", "root");
 		prop.put("password", "root");
 		
-		String sql = "SELECT * FROM food " + 
-				"WHERE name LIKE ? OR addr LIKE ? " + 
-				"OR city LIKE ? OR town LIKE ?";
+		String sql = "SELECT * FROM food LIMIT ?, ?";
 		
-		//String sql = "SELECT * FROM food WHERE name LIKE ? OR addr LIKE ? OR city LIKE ? OR town LIKE ?";
-		// jdbc:sqlserver://localhost:1433;databaseName=AdventureWorks;user=MyUserName;password=*****;encrypt=false;
 		try (Connection conn = DriverManager.getConnection(
 				"jdbc:mysql://localhost/iii", prop);
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 				){
-			pstmt.setString(1, kw);
-			pstmt.setString(2, kw);
-			pstmt.setString(3, kw);
-			pstmt.setString(4, kw);
+			pstmt.setInt(1, kw);
+			pstmt.setInt(2, kw);
 			ResultSet rs = pstmt.executeQuery();
 			while (rs.next()) {
+				String id = rs.getString("id");
 				String name = rs.getString("name");
-				String addr = rs.getString("addr");
-				String city = rs.getString("city");
-				String town = rs.getString("town");
-				System.out.printf("%s:%s:%s:%s\n",name,addr,city,town);
+				System.out.printf("%s:%s\n",id,name);
 			}
 			
 			
