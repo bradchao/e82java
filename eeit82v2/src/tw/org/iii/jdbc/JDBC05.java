61e099f397fe7c9ff770b6145ac2019373049db0
mickeyyo55b <mickeyyo55b@gmail.com>
2024-04-29 10:02:24 +0800

    04291002

--- eeit82v2/src/tw/org/iii/jdbc/JDBC05.java
+++ eeit82v2/src/tw/org/iii/jdbc/JDBC05.java
@@ -5,6 +5,13 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Properties;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class JDBC05 {
 
@@ -26,7 +33,51 @@ public class JDBC05 {
 	}
 	
 	static void parseJSON(String json) {
+		Properties prop = new Properties();
+		prop.put("user", "root");
+		prop.put("password", "root");
 		
+		try (Connection conn = DriverManager.getConnection(
+				"jdbc:mysql://localhost/iii", prop)){
+			
+			String sql = "INSERT INTO food (name,addr,tel,city,town,picurl,lat,lng)" + 
+			" VALUES (?,?,?,?,?,?,?,?)";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			
+			JSONArray root = new JSONArray(json);
+			for (int i=0; i<root.length(); i++) {
+				JSONObject row = root.getJSONObject(i);
+				String name = row.getString("Name");
+				String addr = row.getString("Address");
+				String tel = row.getString("Tel");
+				String city = row.getString("City");
+				String town = row.getString("Town");
+				String picurl = row.getString("PicURL");
+				String lat = row.getString("Latitude");
+				String lng = row.getString("Longitude");
+				
+				pstmt.setString(1, name);
+				pstmt.setString(2, addr);
+				pstmt.setString(3, tel);
+				pstmt.setString(4, city);
+				pstmt.setString(5, town);
+				pstmt.setString(6, picurl);
+				
+				try {
+					pstmt.setDouble(7, Double.parseDouble(lat));
+					pstmt.setDouble(8, Double.parseDouble(lng));
+				}catch(Exception e) {
+					pstmt.setDouble(7, 0.0);
+					pstmt.setDouble(8, 0.0);
+				}
+				
+				pstmt.executeUpdate();
+				
+			}
+			System.out.println("Finish");
+		}catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 	
 	
